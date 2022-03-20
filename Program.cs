81a23bae817685f503db8e1b46734ac6a0bcf4e0
ronitkumarpatel83 <ronitkumarpatel83@gmail.com>
2022-03-20 14:30:40 +0530

    Welcome to my planet

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Basic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Roca");
+            Console.WriteLine("Starting Point");
+            Console.ReadLine();
+        }
+    }
+}
