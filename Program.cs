8ae84c1a0d4d99ca350e84c860657e13cfe6c064
ronitkumarpatel83 <ronitkumarpatel83@gmail.com>
2022-03-20 14:33:24 +0530

    Sifting

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace Basic
         {
             Console.WriteLine("Welcome to Roca");
             Console.WriteLine("Starting Point");
+            Console.WriteLine("Earth to Planet Roca");
             Console.ReadLine();
         }
     }
