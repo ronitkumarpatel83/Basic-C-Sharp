8f63e161a7bf8a443995ee49d7cc66cdafe445cf
Ronit <73175900+ronitkumarpatel83@users.noreply.github.com>
2022-03-20 14:37:56 +0530

    Thanos return

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace Basic
             Console.WriteLine("Welcome to Roca");
             Console.WriteLine("Starting Point");
             Console.WriteLine("Earth to Planet Roca");
+            Console.WriteLine("Thanos has been seen in his Home Planet");
             Console.ReadLine();
         }
     }
